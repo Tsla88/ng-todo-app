b2b4067a45bf91df80181422b2b909cb91134573
DESKTOP-9G5SNPN\alex_ <dexador@mail.ru>
2022-10-06 00:57:54 +0300

    Add model

---
+++ ng-todo-app/src/app/models/Todo.models.ts
@@ -0,0 +1,14 @@
+export class Todo{
+    id:number;
+    title:string;
+    description:string;
+    complited:boolean;
+
+    constructor(id:number,title:string,description:string,complited:boolean){
+this.id=id;
+this.title=title;
+this.description=description;
+this.description=description;
+this.complited=complited;
+    }
+}
